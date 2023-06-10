3b887e4465b877dec3fbbcb2fdb31370b995f9ae
Diogo AA <81387306+Diogo-AA@users.noreply.github.com>
2023-06-10 19:01:32 +0200

    [#1] Arreglado errores en los modelos y en la cache (#15)
    * [#1] Corregido error al buscar un usuario por id y el acceso estatico a la clase Cache
    
    * [#1] Agregado funcion para crear un usuario y agregarlo a la cache
    
    * [#1] Añadido funcion para actualizar wallet
    
    * [#1] Añadido funcion para actualizar wallet
    
    * [#1] Arreglado funcion para obtener el valor actual de una moneda

--- app/Infrastructure/Persistence/FileWalletDataSource.php
+++ app/Infrastructure/Persistence/FileWalletDataSource.php
@@ -5,6 +5,7 @@ namespace App\Infrastructure\Persistence;
 use App\Application\UserDataSource\WalletDataSource;
 use App\Domain\User;
 use App\Domain\Wallet;
+use Exception;
 use Illuminate\Support\Facades\Cache;
 
 class FileWalletDataSource implements WalletDataSource
@@ -14,21 +15,37 @@ class FileWalletDataSource implements WalletDataSource
     {
         $this->cache = new Cache();
     }
+
+    /**
+     * @throws Exception
+     */
     public function create(string $userId): Wallet
     {
         $wallet = new Wallet(intval($userId));
 
-        $this->addWallet($wallet);
+        if (!$this->addWallet($wallet)) {
+            throw new Exception('Error al crear la wallet');
+        }
 
         return $wallet;
     }
+
     public function addWallet(Wallet $wallet): bool
     {
         $wallets = $this->getAll();
-        $wallets[$wallet->getUserId()] = $wallet;
+        $wallets[$wallet->getId()] = $wallet;
 
         return $this->cache::forever("wallets", $wallets);
     }
+
+    public function updateWallet(Wallet $wallet): bool
+    {
+        $wallets = $this->getAll();
+        $wallets[$wallet->getId()] = $wallet;
+
+        return $this->cache::forever("wallets", $wallets);
+    }
+
     public function getAll(): array
     {
         return $this->cache::get("wallets") != null ? $this->cache::get("wallets") : array();
