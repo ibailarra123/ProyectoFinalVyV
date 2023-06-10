512ae51743e13ffba531e06fbdf7de6495e1ae20
Diogo AA <81387306+Diogo-AA@users.noreply.github.com>
2023-06-10 15:23:39 +0200

    [#1] Corregido error al buscar un usuario por id y el acceso estatico a la clase Cache (#9)

--- app/Infrastructure/Persistence/FileUserDataSource.php
+++ app/Infrastructure/Persistence/FileUserDataSource.php
@@ -8,32 +8,38 @@ use Illuminate\Support\Facades\Cache;
 
 class FileUserDataSource implements UserDataSource
 {
+    private Cache $cache;
+    public function __construct()
+    {
+        $this->cache = new Cache();
+    }
     public function addUser(User $user): bool
     {
-        $users = Cache::get("users");
+        $users = $this->cache::get("users");
         $users[$user->getId()] = $user;
 
-        return Cache::forever("users", $users);
+        return $this->cache::forever("users", $users);
     }
     public function getAll(): array
     {
-        return Cache::get("users") != null ? Cache::get("users") : array();
+        return $this->cache::get("users") != null ? $this->cache::get("users") : array();
     }
 
     public function findByEmail(string $email): ?User
     {
-        $users = Cache::get("users");
-        foreach ($users as $user)
-        {
-            if ($user->getEmail() == $email) return $user;
+        $users = $this->cache::get("users");
+        foreach ($users as $user) {
+            if ($user->getEmail() == $email) {
+                return $user;
+            }
         }
         return null;
     }
 
-    public function findById(string $id): ?User
+    public function findById(string $userId): ?User
     {
-        $users = Cache::get("users");
+        $users = $this->cache::get("users");
 
-        return  $users != null ? $users[$id] : null;
+        return  $users != null && array_key_exists($userId, $users) ? $users[$userId] : null;
     }
 }
